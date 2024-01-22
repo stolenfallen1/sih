b4179bbf5e664c5a33941c300176a27eb9e0e1b8
jmestribo <jmestribo.cdg@gmail.com>
2024-01-22 17:11:08 +0800

    add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    build: {
-        transpile: ["vuetify"],
+  app: {
+    head: {
+      title: "CDG HIS",
+      meta: [{ name: "description", content: "CDG HIS" }],
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
     },
-    modules: [
-        "@pinia/nuxt",
-        (_options, nuxt) => {
-            nuxt.hooks.hook("vite:extendConfig", (config) => {
-                // @ts-expect-error
-                config.plugins.push(vuetify({ autoImport: true }));
-            });
-        },
-        //...
-    ],
-    vite: {
-        vue: {
-            template: {
-                transformAssetUrls,
-            },
-        },
+  },
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    "@pinia/nuxt",
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
     },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 });
