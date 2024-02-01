08a8ebbc40f070eb35f17c546bcc250cd984b749
Paw <76894433+stolenfallen1@users.noreply.github.com>
2024-02-01 14:31:44 +0800

    feature: nurse station and report for sys user access ui

--- pages/system-settings/system-users/reports/[id].vue
+++ pages/system-settings/system-users/reports/[id].vue
@@ -1,11 +1,97 @@
 <template>
-   <div>{{ route.params}}3</div>
+    <!-- <div>{{ route.params}}3</div> -->
+    <v-dialog v-model="dialog" persistent hide-overlay width="800" scrollable>
+        <v-card>
+            <v-card-title>System User Reports</v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col v-for="(report, reportIndex) in reports" :key="reportIndex" cols="12" sm="6" md="12">
+                            <h4>{{ report.title }}</h4>
+                            <v-divider></v-divider>
+                            <br/>
+                            <v-expansion-panels>
+                                <v-expansion-panel v-for="(settings, settingsIndex) in report.settings" :key="settingsIndex" :title="settings.settingsPanel">
+                                    <v-expansion-panel-text>
+                                        <v-row v-for="(item, itemIndex) in settings.items" :key="itemIndex">
+                                            <v-checkbox color="#117dad" density="compact" :label="item.label"></v-checkbox>
+                                        </v-row>
+                                    </v-expansion-panel-text>
+                                </v-expansion-panel>
+                            </v-expansion-panels>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn color="blue-darken-1" @click="router.back()"> Close </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn
+                class="bg-primary text-white"
+                type="submit"
+                >Apply</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script setup>
 definePageMeta({
     layout: "root-layout",
 });
+
+const reports = [
+    {
+        title: "Report Settings Group",
+        settings: [
+            {
+                settingsPanel: "Group: Admissions, Emergencies and Outpatients",
+                items: [
+                    { label:  "Reports Settings 1"},
+                ],
+            },
+            {
+                settingsPanel: "Group: Cash Reciepts and Collections",
+                items: [
+                    { label:  "Reports Settings 2"},
+                ],
+            },
+            {
+                settingsPanel: "Group: Customized Reports",
+                items: [
+                    { label:  "Reports Settings 3"},
+                ],
+            },
+            {
+                settingsPanel: "Group: Government Reports",
+                items: [
+                    { label:  "Reports Settings 4"},
+                ],
+            },
+            {
+                settingsPanel: "Group: Master File and Templates",
+                items: [
+                    { label:  "Reports Settings 5"},
+                ],
+            },
+            {
+                settingsPanel: "Group: Requisitions, Charges and Billing Statements",
+                items: [
+                    { label:  "Reports Settings 6"},
+                ],
+            },
+            {
+                settingsPanel: "Group: Others",
+                items: [
+                    { label:  "Reports Settings 7"},
+                ],
+            },
+        ],
+    },
+];
+
+const router = useRouter()
 const route = useRoute()
 
 // When accessing /posts/1, route.params.id will be 1
