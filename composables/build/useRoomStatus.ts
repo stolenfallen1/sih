0f8b32509457f71a7ed0efda65bc3e336c707a8e
jmestribo <jmestribo.cdg@gmail.com>
2024-02-23 17:16:12 +0800

    update

---
+++ composables/build/useRoomStatus.ts
@@ -0,0 +1,14 @@
+import nuxtStorage from 'nuxt-storage';
+export const useRoomStatus = async () => {
+    const config = useRuntimeConfig();
+    const { data}: any = await useFetch(useApiUrl() + `/get-room-status`, {
+    method: 'GET',
+    headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer `+useToken(),
+    },
+    });
+    if (data.value) {
+        nuxtStorage.localStorage.setData('room_status', JSON.stringify(data.value), 24, 'h');
+    }
+}
