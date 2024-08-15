4c10537dca32cbd9704eed278e734015dbd5e13a
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-08-15 15:53:32 +0800

    allergy ui update

--- components/services/forms/out-patient/opd-components/opd-sub-forms/OPDAllergiesList.vue
+++ components/services/forms/out-patient/opd-components/opd-sub-forms/OPDAllergiesList.vue
@@ -40,9 +40,19 @@
                         <template
                             v-for="(head, index) of headers"  v-slot:[`item.${head.value}`]="props" >
                             <td class="test" :props="props" :key="index">
-                            <slot :name="head.value" :item="props.item">
-                                {{ props.item[head.value] || "..." }}
-                            </slot>
+                                <slot :name="head.value" :item="props.item">
+                                    {{ props.item[head.value] || "..." }}
+                                </slot>
+                            </td>
+                        </template>
+                        <template v-slot:[`item.edit`]="props">
+                            <td>
+                                <v-icon color="green" class="cursor-pointer" @click="editItem(props.item)">mdi-pencil</v-icon>
+                            </td>
+                        </template>
+                        <template v-slot:[`item.archive`]="props">
+                            <td>
+                                <v-icon color="red" class="cursor-pointer" @click="archiveItem(props.item)">mdi-archive-arrow-down</v-icon> 
                             </td>
                         </template>
                     </v-data-table-server>
@@ -51,7 +61,7 @@
                 <v-card-actions>
                     <v-btn color="blue-darken-1 border border-info" @click="closeDialog"> Close </v-btn>
                     <v-spacer></v-spacer>
-                    <!-- <v-btn class="bg-info text-white" @click="handleAddAllergy">Add Allergy</v-btn> -->
+                    <v-btn class="bg-info text-white" @click="handleAddAllergy">Add Allergy</v-btn>
                     <v-btn class="bg-primary text-white" @click="openNoteRemarksEntry">Select Allergy</v-btn>
                 </v-card-actions>
             </v-card>
@@ -75,11 +85,22 @@
             <v-card-text>
                 <v-row>
                     <v-col cols="12">
-                        <v-list-subheader class="form-header">Remarks</v-list-subheader>
+                        <v-list-subheader class="form-header">Cause</v-list-subheader>
                         <v-textarea
                             class="cursor-pointer"
-                            placeholder="Enter Patient's Allergy Remarks"
-                            v-model="payload[0].remarks"
+                            placeholder="Enter Patient's Allergy Cause"
+                            v-model="payload[0].cause"
+                            density="compact"
+                            hide-details
+                            variant="solo"
+                        ></v-textarea>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-list-subheader class="form-header">Symptoms</v-list-subheader>
+                        <v-textarea
+                            class="cursor-pointer"
+                            placeholder="Enter Patient's Allergy Symptoms"
+                            v-model="payload[0].symptoms"
                             density="compact"
                             hide-details
                             variant="solo"
@@ -95,7 +116,7 @@
         </v-card>
     </v-dialog>
 
-    <!-- <v-dialog 
+    <v-dialog 
         v-model="add_allergy_dialog"
         @update:model-value="closeAddAllergy"
         hide-overlay
@@ -103,7 +124,7 @@
     >
         <v-card rounded="lg">
             <v-toolbar density="compact" color="#107bac" hide-details>
-                <v-toolbar-title>Add Allergy</v-toolbar-title>
+                <v-toolbar-title>Allergy Form</v-toolbar-title>
                 <v-btn color="white" @click="closeAddAllergy">
                     <v-icon>mdi-close</v-icon>
                 </v-btn>
@@ -112,26 +133,33 @@
             <v-card-text>
                 <v-row>
                     <v-col cols="12">
-                        <v-list-subheader class="form-header">Description</v-list-subheader>
-                        <v-text-field
-                            type="text"
+                        <v-list-subheader class="form-header">Allergy Name</v-list-subheader>
+                        <v-textarea
+                            class="cursor-pointer"
+                            placeholder="Enter Allergy Name here..."
+                            v-model="payload.allergy_name"
                             density="compact"
-                            placeholder="Enter Description"
                             hide-details
                             variant="solo"
-                        ></v-text-field>
+                        ></v-textarea>
                     </v-col>
                 </v-row>
             </v-card-text>
             <v-card-actions>
                 <v-btn color="blue-darken-1 border border-info" @click="closeAddAllergy"> Close </v-btn>
                 <v-spacer></v-spacer>
-                <v-btn class="bg-primary text-white" type="submit" @click="onSubmitAllergy">Continue</v-btn>
+                <v-btn class="bg-primary text-white" type="submit" :loading="isloading" @click="onSubmitAllergy">Submit</v-btn>
             </v-card-actions>
         </v-card>
-    </v-dialog> -->
+    </v-dialog>
 
     <Snackbar />
+    <Confirmation 
+        :show="confirm_password"
+        :payload="password_payload"
+        @close-dialog="closeConfirmPassword"
+        @submit="handleConfirmPassword"
+    />
 </template>
 
 <script setup>
@@ -144,17 +172,21 @@ const props = defineProps({
 })
 
 const emits = defineEmits(['close-dialog', 'handle-select']);
+const confirm_password = ref(false);
 const remarks_form_dialog = ref(false);
 const add_allergy_dialog = ref(false);
 const payload = ref({});
+const password_payload = ref({ user_passcode: "" });
 const selected_item = ref([]);
 const isloading = ref(false);
 const headers = [
     { title: 'ID', align: 'start', sortable: false, key: 'id' },
-    { title: 'Allergy Name', key: 'allergy_name', align: 'start', width:"70%" },
+    { title: 'Allergy Name', key: 'allergy_name', align: 'start', width:"60%" },
+    { title: '', key: 'edit', align: 'start', width: "5%" },
+    { title: '', key: 'archive', align: 'start', width: "5%" },
 ];
 const data = ref({
-    title: "List of Discount Scheme Selection",
+    title: "List of Allergies",
     keyword: "",
     loading: false,
     filter: {},
@@ -198,23 +230,95 @@ const closeRemarksEntry = () => {
     remarks_form_dialog.value = false;
 };
 
-// const handleAddAllergy = () => {
-//     add_allergy_dialog.value = true;
-// };
+const handleAddAllergy = () => {
+    add_allergy_dialog.value = true;
+};
 
-// const onSubmitAllergy = () => {
-//     alert('Allergy added successfully.');
-//     add_allergy_dialog.value = false;
-// }
+const onSubmitAllergy = async () => {
+    try {
+        isloading.value = true;
+        if (payload.value.id) {
+            const response = await useMethod("put", "update-allergy-type", payload.value, "", payload.value.id);
+            if (response) {
+                useSnackbar(true, "green", "Allergy successfully updated.");
+                isloading.value = false;
+                payload.value = "";
+                closeAddAllergy();
+                loadItems();
+            } else {
+                isloading.value = false;
+                useSnackbar(true, "red", "Failed to update allergy.");
+            }
+        } else {
+            if (!payload.value.allergy_name) {
+                return useSnackbar(true, "red", "Please enter an allergy name.");
+            } else {
+                const response = await useMethod("post", "create-allergy-type", payload.value);
+                if (response) {
+                    useSnackbar(true, "green", "Allergy successfully added.");
+                    isloading.value = false;
+                    payload.value = "";
+                    closeAddAllergy();
+                    console.log("asdasda");
+                    loadItems();
+                } else {
+                    isloading.value = false;
+                    useSnackbar(true, "red", "Failed to add allergy.");
+                }
+            }
+        }
+    } catch (error) {
+        isloading.value = false;
+        console.log(error);
+    } 
+}
 
-// const closeAddAllergy = () => {
-//     add_allergy_dialog.value = false;
-// };
+const closeAddAllergy = () => {
+    add_allergy_dialog.value = false;
+    payload.value = Object.assign({});
+};
 
 const handleSelectedRow = (selectedRows) => {
     const selectedItems = selectedRows.map(rowId => serverItems.value.find(item => item.id === rowId));
     const validSelectedItems = selectedItems.filter(item => item !== undefined);
     payload.value = validSelectedItems;
+}
+
+const editItem = (item) => {
+    add_allergy_dialog.value = true;
+    payload.value = Object.assign({}, item);
+}
+
+const archiveItem = async (item) => {
+    payload.value = item;
+    confirm_password.value = true;
+}
+
+const closeConfirmPassword = () => {
+    confirm_password.value = false;
+    password_payload.value = {};
+}
+
+const handleConfirmPassword = async (passcodeData) => {
+    try {
+        isloading.value = true;
+        if (passcodeData.user_passcode === usePasscode()) {
+            const response = await useMethod("put", "archive-allergy-type", "", "", payload.value.id);
+            if (response) {
+                loadItems();
+                closeConfirmPassword();
+                return useSnackbar(true, "green", "Allergy successfully archived.");
+            } else {
+                useSnackbar(true, "error", "Failed to archive allergy.");
+            }
+        } else {
+            useSnackbar(true, "error", "Invalid passcode.");
+        }
+    } catch(error) {
+        console.log(error);
+    } finally {
+        isloading.value = false;
+    }
 }
 
 const onSubmit = () => {
