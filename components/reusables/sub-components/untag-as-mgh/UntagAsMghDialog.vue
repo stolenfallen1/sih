81e6d4e7c85b04935b45ab296545809d5e440da8
Charlie <santandercharlie.mail@gmail.com>
2024-10-15 17:13:51 +0800

    added untag functionality

--- components/reusables/sub-components/untag-as-mgh/UntagAsMghDialog.vue
+++ components/reusables/sub-components/untag-as-mgh/UntagAsMghDialog.vue
@@ -1,9 +1,9 @@
 <template>
     <v-dialog :model-value="show" rounded="lg" scrollable @update:model-value="closeDialog" max-width="600px">
-        <form @submit.prevent="onSubmit">        
+        <form @submit.prevent="openConfirmDialog">        
             <v-card rounded="lg">
                 <v-toolbar density="compact" color="#107bac" hide-details>
-                    <v-toolbar-title>Untag as May Go Home {{ selectedRowDetails.id }}</v-toolbar-title>
+                    <v-toolbar-title>Untag as May Go Home Patient : {{ selectedRowDetails.patient_Id }}</v-toolbar-title>
                     <v-btn color="white" @click="closeDialog">
                         <v-icon>mdi-close</v-icon>
                     </v-btn>
@@ -18,7 +18,7 @@
                             </p>
                         </v-card-text>
                     </v-card>
-                    <v-row class="mt-2">
+                    <!-- <v-row class="mt-2">
                         <v-col cols="12">
                             <v-textarea
                                 label="Remarks"
@@ -28,7 +28,7 @@
                                 hide-details
                             ></v-textarea>
                         </v-col>
-                    </v-row>
+                    </v-row> -->
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-card-actions>
@@ -39,31 +39,98 @@
             </v-card>
         </form>
     </v-dialog>
+
+    <Confirmation 
+        :show="showDialog"
+        :payload="payload"
+        :loading="isLoading"
+        @submit="onSubmit"
+        @close="closeConfirmDialog"
+    />
 </template>
 
 <script setup>
-const props = defineProps({
-    show: {
-        type: Boolean,
-        default: () => false,
-        required: true,
-    },
-});
+    const props = defineProps({
+        show: {
+            type: Boolean,
+            default: () => false,
+            required: true,
+        },
+        form_type: {
+            type: String,
+            default: () => '',
+        },
+    });
 
-const payload = ref({});
+    const showDialog = ref(false);
+    const isLoading = ref(false);
 
-const { selectedRowDetails } = storeToRefs(useSubcomponentSelectedRowDetailsStore()); 
+    const openConfirmDialog = async () => {
+        showDialog.value = true;
+    }
+    const closeConfirmDialog = () => {
+        showDialog.value = false;
+    }
 
-const onSubmit = () => {
-    alert('Submit');
-    emits('close-dialog');
-}
+    const emits = defineEmits(['close-dialog']);
+    
+    const { selectedRowDetails } = storeToRefs(useSubcomponentSelectedRowDetailsStore()); 
 
-const emits = defineEmits(['close-dialog'])
+    const payload = ref([
+        {
+            mgh_queue_no: 0,
+        }
+    ]);
 
-const closeDialog = () => {
-    emits('close-dialog');
-}
+    const closeDialog = () => {
+        emits('close-dialog');
+    }
+
+    const onSubmit = async () => {
+        let response;
+        isLoading.value = true;
+        try{
+            response = await useMethod("put", "untag-patient-maygohome", payload.value, "", payload.value.case_No);
+            if(response) {
+                useSnackbar(true, "green", response.message);
+                isLoading.value = false;
+                closeConfirmDialog();
+            } else {
+                if (error.response && error.response.status === 400) {
+                    useSnackbar(true, "red", response.message || 'Patient has already been discharged.');
+                    isLoading.value = false;
+                } else if(error.response && error.response.status === 404) {
+                    useSnackbar(true, "red", 'Incorrect Username or Passcode');
+                    isLoading.value = false;
+                }
+            }
+        } catch(error) { 
+            if (error.response && error.response.status === 400) {
+                useSnackbar(true, "red", response.message || 'Patient has already been discharged.');
+                isLoading.value = false;
+            } else if(error.response && error.response.status === 404) {
+                useSnackbar(true, "red", 'Incorrect Username or Passcode');
+                isLoading.value = false;
+            }else {  
+                useSnackbar(true, "red", response.message || 'Tagged Failed');
+                isLoading.value = false;
+            }
+        } finally {
+            isLoading.value = false;
+        }
+    }
+
+    onUpdated(() => {
+        if (selectedRowDetails.value && selectedRowDetails.value.id) {
+            if (payload.value.id !== selectedRowDetails.value.id) { 
+                payload.value                   = Object.assign({}, selectedRowDetails.value);
+                payload.value.patient_Id        = selectedRowDetails.value.patient_Id ? selectedRowDetails.value.patient_Id : '';
+                payload.value.suffix_id         = parseInt(selectedRowDetails.value.suffix_id) ? parseInt(selectedRowDetails.value.suffix_id) : '';
+                payload.value.case_No           = selectedRowDetails.value.patient_registry && selectedRowDetails.value.patient_registry[0].case_No ? selectedRowDetails.value.patient_registry[0].case_No : '';
+            }
+        } 
+    })
+
 </script>
 
 <style scoped>
