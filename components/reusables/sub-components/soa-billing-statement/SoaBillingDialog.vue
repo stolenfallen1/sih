7e87362bd5725c3d089fa2797b19510cde527e48
Charlie <santandercharlie.mail@gmail.com>
2024-10-17 08:57:10 +0800

    updated mgh, , utagmgh, discharged

--- components/reusables/sub-components/soa-billing-statement/SoaBillingDialog.vue
+++ components/reusables/sub-components/soa-billing-statement/SoaBillingDialog.vue
@@ -9,6 +9,7 @@ const props = defineProps({
     },
 });
 
+const isLoading = ref(false);
 const { selectedRowDetails } = storeToRefs(useSubcomponentSelectedRowDetailsStore());
 
 const emits = defineEmits(['close-dialog']);
@@ -17,23 +18,49 @@ const closeDialog = () => {
     emits('close-dialog');
 }
 
-const createAccountStatement = async () => {
-    let caseNo = selectedRowDetails.value.patient_registry[0].case_No;
+// const createAccountStatement = async () => {
+//     let caseNo = selectedRowDetails.value.patient_registry[0].case_No;
 
-    const blob = await useMethod("get", `generate-statement-summary/${caseNo}`, "", "");
+//     const blob = await useMethod("get", `generate-statement-summary/${caseNo}`, "", "");
+//     if(blob) {
+//         const blobUrl = URL.createObjectURL(blob);
 
+//         window.open(blobUrl, '_blank');
+//     } else {
+//             useSnackbar(true, "red", error.blob.message || 'Billing is empty');
+//             isLoading.value = false;
+//     }
 
-    const blobUrl = URL.createObjectURL(blob);
+// }
 
-    window.open(blobUrl, '_blank');
+    const createAccountStatement = async () => {
 
-}
+        let caseNo = selectedRowDetails.value.patient_registry[0].case_No;
+        try {
+            const response = await useMethod("get", `generate-statement-summary/${caseNo}`, "", "");
+            console.log(response);
+            if (response) {
+                const blobUrl = URL.createObjectURL(response);
+                window.open(blobUrl, '_blank');
+            } else {
+                useSnackbar(true, "red", response.message || 'Failed to generate statement');
+                isLoading.value = false;
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 404) {
+                useSnackbar(true, "red", error.response.message || 'Billing is empty');
+            } else {
+                useSnackbar(true, "red", error.response.message || 'Failed to generate statement');
+            }
+            isLoading.value = false;
+        }
+    };
 
-onUpdated(() => {
-    if (props.show) {
-        createAccountStatement();
-        closeDialog();
-    }
-});
+    onUpdated(() => {
+        if (props.show) {
+            createAccountStatement();
+            closeDialog();
+        }
+    });
 
 </script>
