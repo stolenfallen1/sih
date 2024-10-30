7bdd76a688294b590d3e703152ecbcfebc538fcb
Charlie <santandercharlie.mail@gmail.com>
2024-10-30 09:40:50 +0800

    updates added display and charge cancelation

--- components/reusables/WarningChargeConfirmation.vue
+++ components/reusables/WarningChargeConfirmation.vue
@@ -6,25 +6,25 @@
         max-width="300px"
       >
         <v-card>
-          <v-divider></v-divider>
-          <v-card-text>
-            <div class="text-center">
-              <v-icon size="80" color="red">mdi-alert</v-icon>
-            </div>
-            <h2 class="mt-3" style="text-align: center;">Warning</h2>
-            <p>Total charges exceed the credit limit</p>
-            <p>Would you like to continue?</p>
-          </v-card-text>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn color="blue darken-1" outlined @click="handleClose">
-              No
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" @click="handleConfirm" outlined>
-              Confirm
-            </v-btn>
-          </v-card-actions>
+          	<v-divider></v-divider>
+          	<v-card-text>
+				<div class="text-center">
+				<v-icon size="80" color="red">mdi-alert</v-icon>
+				</div>
+				<h2 class="mt-3" style="text-align: center;">Warning</h2>
+				<p>Total charges exceed the credit limit</p>
+				<p>Would you like to continue?</p>
+          	</v-card-text>
+			<v-divider></v-divider>
+			<v-card-actions>
+				<v-btn color="blue darken-1" outlined @click="handleClose">
+					No
+				</v-btn>
+				<v-spacer></v-spacer>
+				<v-btn color="blue darken-1" @click="handleConfirm" outlined>
+					Confirm
+				</v-btn>
+			</v-card-actions>
         </v-card>
       </v-dialog>
     </div>
