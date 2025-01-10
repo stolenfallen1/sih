f7effd377a07b2e0ecb4b36dfb2f535e40ac4bc5
Jucel Estribo <jmestribo.cdg@gmail.com>
2025-01-10 10:08:28 +0800

    update

--- composables/useGetUserDetails.ts
+++ composables/useGetUserDetails.ts
@@ -1,17 +1,25 @@
 import nuxtStorage from "nuxt-storage";
 export const useGetUserDetails = (type = null) => {
-  let userdetails = JSON.parse(
-    nuxtStorage.localStorage.getData("user_details")
-  );
-  if (userdetails) {
+     // static sa nko
     if (type == "branch") {
-      return userdetails.branch.name;
-    } else if (type == "address") {
-      return userdetails.branch.address;
-    } else if (type == "TIN") {
-      return userdetails.branch.TIN;
-    } else if (type == "") {
-      return userdetails;
-    }
-  }
+        return 'Cebu Doctors` University Hospital';
+      } else if (type == "address") {
+        return 'Osmeña BLVD, Cebu City, 6000 Cebu';
+      } else if (type == "TIN") {
+        return '000-309-308-000';
+      } 
+
+    // comment sa ni kay sa nginx d mo work
+    //   let userdetails = JSON.parse(nuxtStorage.localStorage.getData("user_details"));
+    //   if (userdetails) {
+    //     if (type == "branch") {
+    //       return userdetails.branch.name;
+    //     } else if (type == "address") {
+    //       return userdetails.branch.address;
+    //     } else if (type == "TIN") {
+    //       return userdetails.branch.TIN;
+    //     } else if (type == "") {
+    //       return userdetails;
+    //     }
+    //   }
 };
